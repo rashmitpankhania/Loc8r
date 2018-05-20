7836a77eb466d3e15f2b893e60d39cda19af84ef
rashmitpankhania <rashmitpankhania@gmail.com>
2018-05-20 17:13:13 +0530

    changes in databse , in reviews always push

--- notes_api/controllers/reviews.js
+++ notes_api/controllers/reviews.js
@@ -3,56 +3,59 @@ var Loc = mongoose.model('Location');
 
 var sendJsonResponse = function (res, status, content) {
 	res.status(status);
-	res.set({'Content-Type': 'application/json; charset=utf-8'}).status(200).send(JSON.stringify(content, undefined, '\t'));
+	res.set({ 'Content-Type': 'application/json; charset=utf-8' }).status(200).send(JSON.stringify(content, undefined, '\t'));
 };
 
 module.exports.reviewCreate = function (req, res) {
 
 };
 module.exports.reviewRead = function (req, res) {
-	if (req.params && req.params.locationid && req.params.reviewid){
-		Loc.findById(req.params.locationid).select('name reviews').exec(function (err, location) {
-			var response, review;
-			if (!location) {
-				sendJsonResponse(res, 404, {
-					'message': 'Dude thats not here'
+	if (req.params && req.params.locationid && req.params.reviewid) {
+		Loc
+			.findById(req.params.locationid)
+			.exec(
+				function (err, location) {
+					var response, review;
+					if (!location) {
+						sendJsonResponse(res, 404, {
+							'message': 'Dude thats not here'
+						});
+						return;
+					} else if (err) {
+						sendJsonResponse(res, 404, err);
+						return;
+					}
+					if (location.reviews && location.reviews.length > 0) {
+						review = location.reviews.id(req.params.reviewid);
+						console.log(review)
+						if (!review) {
+							sendJsonResponse(res, 404, {
+								'message': 'wth reviewid not found'
+							});
+						} else {
+							response = {
+								location: {
+									name: location.name,
+								},
+								review: review
+							};
+							sendJsonResponse(res, 200, response);
+						}
+					} else {
+						sendJsonResponse(res, 404, {
+							'message': 'review not found'
+						});
+					}
 				});
-				return;
-			} else if (err) {
-				sendJsonResponse(res, 404, err);
-				return;
-			}
-			if (location.reviews && location.reviews.length > 0) {
-				review = location.reviews.id(req.params.reviewid);
-				if(!review) {
-					sendJsonResponse(res, 404, {
-						'message': 'wth reviewid not found'
-					});
-				} else {
-					response = {
-						location : {
-							name: location.name,
-							id: req.params.locationid
-						},
-						review: review
-					};
-					sendJsonResponse(res, 200, response);
-				}
-			} else {
-				sendJsonResponse(res, 404, {
-					'message': 'review not found'
-				});
-			}
-		});
 	}
 	else {
-		sendJsonResponse(res, 404, {'message': 'Dude i  know don\'t be smart.. no id in request'})
+		sendJsonResponse(res, 404, { 'message': 'Dude i  know don\'t be smart.. no id in request' })
 	}
-	
+
 };
 module.exports.reviewUpdate = function (req, res) {
-	
+
 };
 module.exports.reviewDelete = function (req, res) {
-	
+
 };
